0a7cb97f61b3cc14b34af4391a1175da3f47ad07
joaochioda <joao.chioda@dextra-sw.com>
2020-02-22 16:52:09 -0300

    test removing functions

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<span>
-					Oi, meu nome é afeseila
+					Oi, meu nome é afeseilass
 				</span>
 			</header>
 		</div>
