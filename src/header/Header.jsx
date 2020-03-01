15b9d9c79eb278de683c968fa2915e089b09a3f6
joaochioda <joao.chioda@dextra-sw.com>
2020-03-01 19:11:39 -0300

    creating main

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
-import { Div, Li, Ul } from '../components/HeaderCss';
+import { Div, Ul, Joao, Guilherme, Li, Nav } from '../components/HeaderCss';
 import { useTranslation } from 'react-i18next';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default function Header() {
 	const { t } = useTranslation();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const handleClick = event => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
-		<Div>
-			<Ul>
-				<Li>{t('header.whoami')}</Li>
-				<Li>{t('header.projects')}</Li>
-				<Li>{t('header.contact')}</Li>
-			</Ul>
-		</Div>
+		<div>
+
+			<Nav>
+				<Joao>
+					<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+						{t('header.menu')}
+					</Button>
+					<Menu
+						id="simple-menu"
+						anchorEl={anchorEl}
+						keepMounted
+						open={Boolean(anchorEl)}
+						onClose={handleClose}
+					><MenuItem onClick={handleClose}>{t('header.whoami')}</MenuItem>
+						<MenuItem onClick={handleClose}>{t('header.projects')}</MenuItem>
+						<MenuItem onClick={handleClose}>{t('header.contact')}</MenuItem>
+					</Menu>
+				</Joao>
+				<Guilherme>
+					<Div>
+						<Ul>
+							<Li>{t('header.whoami')}</Li>
+							<Li>{t('header.projects')}</Li>
+							<Li>{t('header.contact')}</Li>
+						</Ul>
+					</Div>
+				</Guilherme>
+			</Nav>
+		</div>
+
 	);
 }
---
+++ src/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Div } from '../components/MainCss';
+export default function BackGround() {
+
+	return (
+		<Div>
+            oi
+		</Div>
+	);
+
+}
