26115694669564026224df5a7fc2a2a7c533bd62
joaochioda <joao.chioda@dextra-sw.com>
2020-03-17 16:48:53 -0300

    body parameters

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,6 @@ const nodemailer = require('nodemailer');
 const cors = require('cors')({origin: true});
 admin.initializeApp();
 
-/**
-* Here we're using Gmail to send 
-*/
 let transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
@@ -18,26 +15,26 @@ let transporter = nodemailer.createTransport({
 exports.sendMail = functions.https.onRequest((req, res) => {
 	cors(req, res, () => {
       
-		// getting dest email by query string
-		const body = req.body;
-		const dest = req.query.dest;
-		console.log(body);
+		const body = JSON.stringify(req.body);
+		const data = JSON.parse(body);
+	
 		const mailOptions = {
-			from: 'Your Account Name <joaoportfolio123@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
-			to: dest,
-			subject: 'I\'M A PICKLE!!!', // email subject
-			html: `<p style="font-size: 16px;">Pickle Riiiiiiiiiiiiiiiick!!</p>
-                <br />
-                <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
-            ` // email content in HTML
+			from: 'Portfolio <joaoportfolio123@gmail.com>',
+			to: 'joao.chioda@dextra-sw.com',
+			subject: data.subject,
+			html: `<p style="font-size: 16px;">${data.email}</p>
+				<br />
+				<p>${data.message}</p>
+				<br />
+				<p>${data.name}</p>
+            `
 		};
   
-		// returning result
 		return transporter.sendMail(mailOptions, (erro, info) => {
 			if(erro){
 				return res.send(erro.toString());
 			}
 			return res.send('Sended');
 		});
-	});    
+	});
 });
